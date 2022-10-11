a6e60b5cdf1f103b01c480d5e5c987d0ce018c14
Sanyam Kamat <sanyamkamat@gmail.com>
2022-10-11 13:08:25 +0530

    add partytown

---
+++ src/components/partytown/partytown.tsx
@@ -0,0 +1,20 @@
+import { PartytownConfig, partytownSnippet } from "@builder.io/partytown/integration";
+
+/**
+ * Props for `<QwikPartytown/>`, which extends the Partytown Config.
+ *
+ * https://github.com/BuilderIO/partytown#config
+ *
+ * @public
+ */
+export interface PartytownProps extends PartytownConfig {
+}
+
+/**
+ * @public
+ * You can pass setting with props
+ */
+export const QwikPartytown = (props: PartytownProps): any => {
+  const innerHTML = partytownSnippet(props);
+  return <script dangerouslySetInnerHTML>{innerHTML}</script>;
+};
